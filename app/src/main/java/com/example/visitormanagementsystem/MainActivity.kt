784b474b7b9b32c93aa6392e53ed8bc9362562d1
Shashi <shashikumar.choudhry@gmail.com>
2023-04-18 01:29:23 +0530

    Project Updated

--- app/src/main/java/com/example/visitormanagementsystem/MainActivity.kt
+++ app/src/main/java/com/example/visitormanagementsystem/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.visitormanagementsystem
 
 import android.app.DatePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var entryDate : EditText
-    private lateinit var exitDate : EditText
-    private lateinit var fname : EditText
-    private lateinit var lname : EditText
-    private lateinit var email: EditText
-    private lateinit var phoneNo : EditText
+    private lateinit var entryDate : TextView
+    private lateinit var exitDate : TextView
+
+    private lateinit var fname : TextView
+    private lateinit var lname : TextView
+    private lateinit var email: TextView
+    private lateinit var phoneNo : TextView
 
     private lateinit var wheel2 : CheckBox
     private lateinit var wheel3 : CheckBox
@@ -23,6 +28,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var submit : Button
     private lateinit var clear : Button
+
+    lateinit var sharedPreferences: SharedPreferences
+    private val myPreference = "mypref"
+    private val EntryDate = "entryDateKey"
+    private val ExitDate = "exitDateKey"
+    private val FirstName = "fNameKey"
+    private val LastName = "lNameKey"
+    private val Email = "emailKey"
+    private val PhoneNo = "phoneKey"
+//    private val Wheel2 = "wheel2Key"
+//    private val Wheel3 = "wheel3Key"
+//    private val Wheel4 = "wheel4Key"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,11 +59,64 @@ class MainActivity : AppCompatActivity() {
         submit = findViewById(R.id.submitBtn)
         clear = findViewById(R.id.clearBtn)
 
-        fillEntryDate(entryDate)
-        fillEntryDate(exitDate)
+        fillEntryDate(entryDate as EditText)
+        fillEntryDate(exitDate as EditText)
         clearFields()
 
+        sharedPref()
+
     }
+
+    private fun sharedPref() {
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+        entryDate.text = sharedPreferences.getString(EntryDate, "")
+        exitDate.text = sharedPreferences.getString(ExitDate, "")
+        fname.text = sharedPreferences.getString(FirstName, "")
+        lname.text = sharedPreferences.getString(LastName, "")
+        email.text = sharedPreferences.getString(Email, "")
+        phoneNo.text = sharedPreferences.getString(PhoneNo, "")
+
+//        wheel2.isChecked = sharedPreferences.getBoolean(Wheel2, false)
+//        wheel3.isChecked = sharedPreferences.getBoolean(Wheel3, false)
+//        wheel4.isChecked = sharedPreferences.getBoolean(Wheel4, false)
+
+    }
+
+    fun save(view: View?) {
+        val enterDt = entryDate.text.toString()
+        val exitDt = exitDate.text.toString()
+        val fName = fname.text.toString()
+        val lName = lname.text.toString()
+        val eml = email.text.toString()
+        val phone = phoneNo.text.toString()
+//        val whl2 = wheel2.
+
+
+        val editor = sharedPreferences.edit()
+        editor.putString(EntryDate, enterDt)
+        editor.putString(ExitDate, exitDt)
+        editor.putString(FirstName, fName)
+        editor.putString(LastName, lName)
+        editor.putString(Email, eml)
+        editor.putString(PhoneNo, phone)
+
+        editor.apply()
+    }
+
+    fun get(view: View?) {
+
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+        entryDate.text = sharedPreferences.getString(EntryDate, "")
+        exitDate.text = sharedPreferences.getString(ExitDate, "")
+        fname.text = sharedPreferences.getString(FirstName, "")
+        lname.text = sharedPreferences.getString(LastName, "")
+        email.text = sharedPreferences.getString(Email, "")
+        phoneNo.text = sharedPreferences.getString(PhoneNo, "")
+
+
+    }
+
+
 
     private fun fillEntryDate(time : EditText) {
         val calendar = Calendar.getInstance()
